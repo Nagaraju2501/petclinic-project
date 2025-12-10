a753fa949ef51fa86414b3d321c4f4478af59e26
Nagaraju2501 <nagarajutadi12@gmail.com>
2025-12-10 11:17:56 +0000

    jenkins.sh file updated

--- jenkins.sh
+++ jenkins.sh
@@ -1,13 +1,14 @@
-sudo apt-get update
-:
-curl -fsSL https:/jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkinsen.io/debian-stable binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null
-
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
+sudo apt install jenkins
 sudo apt install openjdk-17-jdk
 sudo apt install openjdk-17-jre
 
-sudo systemctl enable j
+sudo apt install jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
